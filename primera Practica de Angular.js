76882925815be2663bbf7011d22b871e3350232d
dalwin <dalwin1313@gmail.com>
2017-07-16 18:33:23 -0400

    Update and rename primera Practica de Angular to primera Practica de Angular.js

---
+++ primera Practica de Angular.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict'
+
+var var1, var2, var3,var4,var5,var6,var7;
+
+var1 =  "Primera Practica de Angular";
+var2 =  " ";
+var3 =  "Dalwin Cuello Bueno";
+var4 =  "Estudiante y Desarrollador";
+var5 =  "27";
+var6 =  "5";
+var7 =  "Aprender un nuevo lenguaje de programacion y ampliar mas conocimientos en programacion.";
+
+console.log(var1);
+console.log(var2);
+console.log("Nombre : ",var3);
+console.log(var2);
+console.log("Profecion : ",var4);
+console.log(var2);
+console.log("Edad : ",var5);
+console.log(var2);
+console.log("Nivel de conocimientos de programación del 1 al 10: ",var6);
+console.log(var2);
+console.log("Expectativas que tienes del curso : ",var7);
+
+
+})();
