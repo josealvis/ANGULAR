1d10faa3a1280573668fa79b52c4340d57ca5011
José Alvis <jalvis1491@gmail.com>
2017-07-17 15:17:30 -0400

    Buen trabajo
    Buen trabajo. Solo te puedo recomendar que utilices nombres de variables que describan el contenido de la variable.
    Es buena práctica, por ejemplo que una variable que  va a contener una edad se llame edad.
    Esto ayuda a que el código sea mas fácil de entender.

--- practica1_angular.js
+++ practica1_angular.js
@@ -1,27 +1,27 @@
 (function() {
     'use strict'
 
-var var1, var2, var3,var4,var5,var6,var7;
+var titulo, nombre, ocupacion,edad, nivelJavascript ,mensajeExpectativas;
 
-var1 =  "Primera Practica de Angular";
-var2 =  " ";
-var3 =  "Dalwin Cuello Bueno";
-var4 =  "Estudiante y Desarrollador";
-var5 =  "27";
-var6 =  "5";
-var7 =  "Aprender un nuevo lenguaje de programacion y ampliar mas conocimientos en programacion.";
+titulo =  "Primera Practica de Angular";
+//var2 =  " ";  no es necesario utilizar una variable para imprimir un espacio.
+nombre =  "Dalwin Cuello Bueno";
+ocupacion =  "Estudiante y Desarrollador";
+edad =  "27";
+nivelJavascript =  "5";
+mensajeExpectativas =  "Aprender un nuevo lenguaje de programacion y ampliar mas conocimientos en programacion.";
 
-console.log(var1);
-console.log(var2);
-console.log("Nombre : ",var3);
-console.log(var2);
-console.log("Profecion : ",var4);
-console.log(var2);
-console.log("Edad : ",var5);
-console.log(var2);
-console.log("Nivel de conocimientos de programación del 1 al 10: ",var6);
-console.log(var2);
-console.log("Expectativas que tienes del curso : ",var7);
+console.log(titulo);
+console.log();//imprime una linea vacia
+console.log("Nombre : ",nombre);
+console.log();
+console.log("Profecion : ",ocupacion);
+console.log();
+console.log("Edad : ",edad);
+console.log();
+console.log("Nivel de conocimientos de programación del 1 al 10: ",nivelJavascript);
+console.log();
+console.log("Expectativas que tienes del curso : ",mensajeExpectativas);
 
-
+//visto
 })();
